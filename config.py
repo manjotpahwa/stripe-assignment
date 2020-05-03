d85c4cfd6d1865d9528e6e56c7f6891db3e7a5f3
Manjot Pahwa <manjotpahwa@gmail.com>
2020-05-03 22:58:38 +0530

    take in config setting svia environment variables as wel

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # Stripe config options
+#
+import os
 
-STRIPE_API_KEY = "sk_test_edhurKZR5OMK0Wvl7uYLyu1n"
-WEBHOOK_SECRET = "whsec_K8reb65yXmHSEyKkSMx8h84rozwb5R69"
+STRIPE_API_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY') or "sk_test_edhurKZR5OMK0Wvl7uYLyu1n"
+WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET') or "whsec_K8reb65yXmHSEyKkSMx8h84rozwb5R69"
